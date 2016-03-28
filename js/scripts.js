c993d1b26e3d391182eebd464219939ff0d1a776
jciarallij <jciarallij@gmail.com>
2016-03-28 09:44:07 -0400

    init commit
    first angular.js exercise

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// --------------------- Javascript Way ------------------------
+// Vanilla Javascript way
+
+// var myInput = document.getElementById('myInput');
+
+// myInput.addEventListener('keyup', function(){
+// 	// console.log('User hit a key!');
+// 	var userText = myInput.value;
+// 	document.getElementById('inputText').innerHTML = userText
+// })
+// --------------------- Jquery Way ------------------------
+// Jquery Way
+
+// $('#myInput').keyup(function(){
+//  $('#inputText').html($('#myInput').val());
+// })
+
+
